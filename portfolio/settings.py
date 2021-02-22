6c16efb6c8a0e8a8450629c82f440b0106899743
Jennifer Cunningham <cptcunningham@gmail.com>
2021-02-22 15:33:07 -0800

    added ignore and path info

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from my_secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hf40e)uhd%b5nca45gbau%j-!hle&-j%k4ituov=-k=_(610gv'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
