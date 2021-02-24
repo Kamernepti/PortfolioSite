215156818f984a855c4ece1e0710b568f0e792df
Jennifer Cunningham <cptcunningham@gmail.com>
2021-02-23 18:15:53 -0800

    removed extra templates- one page portfolio now, made nav buttons functional

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -2,9 +2,3 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render (request, 'index.html')
-
-def about(request):
-    return render(request, 'about.html')
-
-def contact(request):
-    return render(request, 'contact.html')
