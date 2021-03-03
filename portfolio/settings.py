3ed285be8533bc57ddb0b581d2a57114bcd49fcb
Jennifer Cunningham <cptcunningham@gmail.com>
2021-03-02 16:34:16 -0800

    static url changed and updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY= os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['18.191.245.67']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,5 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_URL = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL='/static/'
-STATIC_URL = os.path.join(BASE_DIR, "static/")
+
